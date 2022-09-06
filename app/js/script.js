02f5d2a95a21300c3218b67959781163d377379d
Simon Reinhardt <saadabou.study@gmail.com>
2022-09-06 14:28:18 +0100

    click detection for test, 3 cells animated, 4  remaining

--- app/js/script.js
+++ app/js/script.js
@@ -2,16 +2,19 @@ const htmlWheel = document.getElementById('htmlWheel');
 let htmlWheelElements = htmlWheel.children;
 
 let main = document.getElementById("main");
+let cell3 = document.getElementById("cell3")
+let cell5 = document.getElementById("cell5")
 
-var i = "test";
 
-document.onkeydown = function (e) {
-  switch (e.keyCode) {
-    case 38:
-      console.log("up"); //show the message saying up"
-      break;
-    case 40:
-      console.log("down"); //show the message saying down"
-      break;
-  }
-};
+htmlWheel.addEventListener("click", () => {
+  main.classList.add("scrolldown-main");
+  cell3.classList.add("scrolldown-cell3");
+  cell5.classList.add("scrolldown-cell5");
+
+  setTimeout(() => {
+    main.classList.remove("scrolldown-main");
+    cell3.classList.remove("scrolldown-cell3");
+    cell5.classList.remove("scrolldown-cell5");
+
+  }, 500);
+})
